b53bfecc2585559c5b28a4bffcd1fae5c75d1384
Abdul Rehman Khan <arkspeed@gmail.com>
2017-02-21 01:28:40 +0530

    7. Naive Bayes giving 60% accuracy only.

--- Model_Training_Prediction.py
+++ Model_Training_Prediction.py
@@ -1,2 +1,25 @@
+#NaiveBayes
+from Data_Preprocessing import *
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix
 
+#Splitting the dataset into Training set and Test set
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
+
+#Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#Fitting Naive Bayes to the Training Set
+clf = GaussianNB()
+clf.fit(X_train, Y_train)
+
+#Predicting the Test set results
+Y_pred = clf.predict(X_test)
+
+#Making the confusion matrix
+cm = confusion_matrix(Y_test, Y_pred)
 
